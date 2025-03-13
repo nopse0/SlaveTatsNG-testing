b85f1ca962813b1f076a9bcabdd18d67f76e9009
nopse0 <145904664+nopse0@users.noreply.github.com>
2025-03-13 16:59:19 +0100

    prepare-headers.bat instead of copy

---
+++ interface/prepare-headers.bat
@@ -0,0 +1,3 @@
+copy ..\extern\openvr\openvr.h ..\extern\CommonLibSSE-NG\include
+set skyrimdir=D:\Games\Skyrim-1.6.1170\overwrite
+copy %skyrimdir%\SKSE\plugins\SlaveTatsNG\SlaveTatsNG_Interface.h include
